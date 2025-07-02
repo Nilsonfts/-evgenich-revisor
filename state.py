bb50275c3717d4d7f8cac660219a9d8a4cfaa312
Nilsonfts <nil.market58@gmail.com>
2025-07-02 21:44:22 +0300

    Update state.py

--- state.py
+++ state.py
@@ -1,1 +1,9 @@
+# state.py
+from typing import Dict, List
 
+# Глобальные переменные, хранящие состояние бота в реальном времени
+chat_data: Dict[int, dict] = {}
+user_history: Dict[int, List[str]] = {}
+chat_configs: Dict[int, dict] = {}
+ad_templates: Dict[str, dict] = {}
+user_states: Dict[int, dict] = {}
